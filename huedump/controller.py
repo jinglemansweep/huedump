978ef394c3a0630e2cce1e1800ad107ccb2e3aed
JingleManSweep <jinglemansweep@gmail.com>
2015-12-08 13:42:23 +0000

    Added templates

--- huedump/controller.py
+++ huedump/controller.py
@@ -1,10 +1,11 @@
 import json
+import os
+import sys
 from collections import OrderedDict
-
 from cement.core.foundation import CementApp
 from cement.core.controller import CementBaseController, expose
 from cement.core import handler
-
+from jinja2 import Environment, FileSystemLoader
 from phue import Bridge
 from prettytable import PrettyTable as Table
 from pygments import highlight, lexers, formatters
@@ -20,21 +21,23 @@ class Controller(CementBaseController):
               dict(action="store", help="hostname of Hue bridge")),
             (["-u", "--user"],
               dict(action="store", help="username of Hue user")),
-            (["-t"],
-              dict(action="store", help="output template")),
+            (["-t", "--template"],
+              dict(action="store", help="template for output rendering", default="")),
             ]
+
 
     @expose(help="Dump")
     def dump(self):
-        self.app.log.info("Dump")
+        self.app.log.debug("Dump")
         self.login()
-        self.app.log.info(self.format_json(self.bridge.get_api()))
+        self.app.log.info(self.format_json(self.api))
 
     @expose(help="Lights")
     def lights(self):
-        self.app.log.info("Lights")
+        self.app.log.debug("Lights")
         self.login()
-        lights = self.sort_dict(self.api().get("lights"))
+
+        lights = self.sort_dict(self.api.get("lights"))
         rows = [[i, 
                  l.get("name"), 
                  l.get("manufacturername"),
@@ -48,16 +51,25 @@ class Controller(CementBaseController):
         t = self.table(["#", "Name", "Manu", "Model", "On", "Bri", "CM", "Hue", "Sat"], rows)
         self.app.log.info(t)
             
+    @expose(help="Render state using a template")
+    def render(self):
+        self.app.log.debug("Render")
+        self.login()
+        tpl = self.template(self.app.pargs.template, self.api)
+        print(tpl)
+
     @expose(hide=True)
     def login(self):
         host = self.app.pargs.bridge or self.app.config.get("bridge", "host")
         user = self.app.pargs.user or self.app.config.get("bridge", "user")
         self.bridge = Bridge(host, user)
         self.bridge.connect()
-
-    @expose(hide=True)
-    def api(self):
-        return self.bridge.get_api()
+        try:
+            self.api = self.bridge.get_api()
+        except Exception, e:
+            if e.errno == -2:
+                self.app.log.error("Invalid bridge host")
+            sys.exit(1)
 
     @expose(hide=True)
     def sort_dict(self, d):
@@ -70,6 +82,13 @@ class Controller(CementBaseController):
             table.add_row(row)
         table.align = "l"
         return "\n{}".format(table)
+
+    @expose(hide=True)
+    def template(self, tpl_path, context):
+        path, filename = os.path.split(tpl_path)
+        return Environment(
+            loader=FileSystemLoader(path or "./")
+        ).get_template(filename).render(context)
 
     @expose(hide=True)
     def format_json(self, json_value):
